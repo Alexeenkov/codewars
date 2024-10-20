227af8731eef9fe40b93294ad2917a09825aca60
alekseenkov.ilya <alexeenkov-frontend@yandex.ru>
2024-10-20 15:42:10 +0300

    "Who likes it" completed

---
+++ Who likes it/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {useWhoLikesString} from "@/composables/useWhoLikesString.js";
+import {computed, ref, unref} from "vue";
+
+const {getWhoLikesString} = useWhoLikesString();
+
+const inputText = ref('');
+const result = computed(() => unref(inputText) && getWhoLikesString(unref(inputText).split(', ')));
+</script>
+
+<template>
+  <main>
+	  <p>Введите имена с пробелом после запятой:</p>
+	  <input v-model="inputText" type="text" placeholder="Alex, Jacob, Mark, Max">
+	  <p> {{ result }} </p>
+  </main>
+</template>
+
